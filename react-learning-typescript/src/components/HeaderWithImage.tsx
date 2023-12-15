3198e2e6c41a33ea6eea17df2aa868ae3aec78b8
Saurabh Verma <saurabh1088@gmail.com>
2023-12-15 23:19:23 +0530

    Added comment about usage of {props.children}

--- react-learning-typescript/src/components/HeaderWithImage.tsx
+++ react-learning-typescript/src/components/HeaderWithImage.tsx
@@ -8,6 +8,24 @@ type HeaderWithImageProps = {
     children: ReactNode
 };
 
+/* 
+Here concept of {props.children} is used to create a generic header component.
+
+In general the role of {props.children} is to create a generic template component that can be modified by the parent when 
+it is invoked. Now as per example below when HeaderWithImage will be used, the parent can pass whatever it needs and the
+passed content is rendered accordingly.
+
+How the children is passed from parent?
+
+Well when a component using {props.children} is used then instead of invoking it with a self-closing tag one needs to invoke
+with a standard open/closing tag, within those tags one places the children component. For example :
+
+<HeaderWithImage image={ {src: reactLogo, alt: "A logo for header"} }>
+    <h1>Header with image</h1>
+</HeaderWithImage>
+
+
+*/
 export default function HeaderWithImage(props: HeaderWithImageProps) {
     return (<header>
         <img src={props.image.src} alt={props.image.alt}/>
