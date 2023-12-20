1c12026390a71b0c3ee877efd19d027197ebe24a
Saurabh Verma <saurabh1088@gmail.com>
2023-12-20 22:33:21 +0530

    Added comment for React Hooks

--- react-learning-typescript/src/components/ButtonWithState.tsx
+++ react-learning-typescript/src/components/ButtonWithState.tsx
@@ -11,6 +11,18 @@ the state given from `color` will be `black`. Next when page is rendered and a b
 set to either `red`, `blue` or `yellow` as per the button which was clicked.
 The first value in the destructured array as discussed above is the state and the second value in the array is the set 
 function which lets one update the state to different value and will trigger a re-render.
+So basically useState gives back the current state and the funtion which will update the state.
+
+In React funtions starting with `use` are called `Hooks`. `useState` used in example below is a built-in hook provided by
+React framework.
+
+What makes Hooks different from funtions?
+
+Hooks are more restrictive than other functions.
+State, before React version 16.8 one needed to handle state only using class components. With version 16.8 Hooks was
+introduced, with React Hooks, we can use state, and other React features, in a functional component. It empowers one to 
+do functional programming in React.
+
 */
 export default function ButtonWithState() {
     const [color, setColor] = useState("black");
